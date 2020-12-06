810bc527d4f30c8aedcd7284ceed3f1cb6e55ae1
Tom Johnston <tomo.johnston.slbs@gmail.com>
2020-12-06 23:39:30 +0000

    completed day 1

---
+++ Day 1/code/challenge1.py
@@ -0,0 +1,10 @@
+file = "../files/input.txt"
+list_expenses = []
+with open(file) as f:
+    for line in f:
+        list_expenses.append(int(line.strip()))
+
+for i in range(len(list_expenses)):
+    for j in range(i, len(list_expenses)):
+        if list_expenses[i] + list_expenses[j] == 2020:
+            print(list_expenses[i]*list_expenses[j])
